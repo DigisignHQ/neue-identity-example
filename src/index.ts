643fd6333aa7aa12b623d6b13c43e98cbacacb8e
cmion <akpanudosimeon@hotmail.com>
2026-04-05 01:28:14 +0100

    chore: moved to bun

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+
+import { auth } from "./lib/auth";
+
+const app = new Elysia()
+  .use(
+    cors({
+      origin: "http://localhost:3001",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+  )
+  .mount(auth.handler)
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
